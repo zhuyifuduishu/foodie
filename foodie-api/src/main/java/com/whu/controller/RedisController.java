0e876b336be3cb7b22b17a5e6b8755b667c67572
hwh <1328371390@qq.com>
2021-08-05 14:18:54 +0800

    搭建redis主从复制，搭建redis三主三从架构

--- foodie-api/src/main/java/com/whu/controller/RedisController.java
+++ foodie-api/src/main/java/com/whu/controller/RedisController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
 
+import java.util.Arrays;
+import java.util.List;
+
 @ApiIgnore
 @RestController
 @RequestMapping("/redis")
@@ -20,23 +23,37 @@ public class RedisController {
     private RedisOperator redisOperator;
 
     @GetMapping("/set")
-    public Object set(String key,String value){
+    public Object set(String key, String value) {
         //redisTemplate.opsForValue().set(key,value);
-        redisOperator.set(key,value);
+        redisOperator.set(key, value);
         return "OK";
     }
 
     @GetMapping("/get")
-    public String get(String key){
+    public String get(String key) {
         //return (String) redisTemplate.opsForValue().get(key);
 
         return redisOperator.get(key);
     }
 
     @GetMapping("/delete")
-    public Object delete(String key){
+    public Object delete(String key) {
         //redisTemplate.delete(key);
         redisOperator.del(key);
         return "OK";
+    }
+
+    @GetMapping("/mget")
+    public Object mget(String... keys) {
+        //return (String) redisTemplate.opsForValue().get(key);
+        List<String> keysList = Arrays.asList(keys);
+
+        return redisOperator.mget(keysList);
+    }
+
+    @GetMapping("/batchGet")
+    public Object batchGet(String... keys) {
+        List<String> keysList = Arrays.asList(keys);
+        return redisOperator.batchGet(keysList);
     }
 }
